b8467246a040e22c844bfecc0c9a1566991e18bf
menilik <spyxmeni@gmail.com>
2023-05-07 00:54:44 +0300

    add user info, contact, diseas to admin dashbord

--- healthpilot/client/admin.py
+++ healthpilot/client/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, EmergencyContact, Disease, Tag, Category, Article
 
 admin.site.site_header = "Health Pilot ADMIN"
 admin.site.site_title = "Health Pilot Admin Portal"
@@ -11,3 +11,23 @@ class CommunityAdmin(admin.ModelAdmin):
     list_display = [field.name for field in User._meta.fields]
     search_fields = ['id', 'username']
     list_filter = ['gender']
+
+@admin.register(EmergencyContact)
+class EmergencyContactAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in EmergencyContact._meta.fields]
+
+@admin.register(Disease)
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Disease._meta.fields]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Tag._meta.fields]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Category._meta.fields]
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Article._meta.fields]
