58958608a3927757b26d37bda48d7a20427ff73a
menilik <spyxmeni@gmail.com>
2023-05-07 00:55:35 +0300

    add api for models

--- healthpilot/client/urls.py
+++ healthpilot/client/urls.py
@@ -6,11 +6,25 @@ from .views import *
 
 router = DefaultRouter()
 router.register('users', UserAPIView, basename='users')
+router.register('emergency_contact', UserAPIView, basename='emergency')
+router.register('disease', UserAPIView, basename='disease')
+router.register('article', UserAPIView, basename='articles')
 
-# users_router = NestedDefaultRouter(router, 'client', lookup='client')
-# users_router.register('users', UserAPIView, basename='client-users')
+
+emergency_contact_router = NestedDefaultRouter(router, 'emergency_contact', lookup='user')
+emergency_contact_router.register('emergency_contact', UserAPIView, basename='emergency_contact')
+
+disease_router = NestedDefaultRouter(router, 'disease', lookup='user')
+disease_router.register('disease', UserAPIView, basename='disease')
+
+article_router = NestedDefaultRouter(router, 'article', lookup='user')
+article_router.register('article', UserAPIView, basename='articles')
+
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('', include(users_router.urls)),
+    path('', include(emergency_contact_router.urls)),
+    path('', include(disease_router.urls)),
+    path('', include(article_router.urls)),
+    
 ]
