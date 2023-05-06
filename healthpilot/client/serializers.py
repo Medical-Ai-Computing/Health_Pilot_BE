607eced219464a7a4fba47be846655adb6bc8c59
menilik <spyxmeni@gmail.com>
2023-05-07 00:55:26 +0300

    add serializers for model api

--- healthpilot/client/serializers.py
+++ healthpilot/client/serializers.py
@@ -1,10 +1,47 @@
 from django.db.models import Q
-from .models import User
+from .models import User, Article, Disease, EmergencyContact, Category
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     '''Used to get and update users'''
     class Meta:
         model = User
-        fields = ['id','nick_name', 'full_name', 'date_of_birth', 'age', 'joined_at', 'gender']
+        fields = ['id','username', 'full_name', 'date_of_birth', 'age', 'gender', 'membership']
         read_only_fields = ['id']
+
+class EmergencyContactSerializer(serializers.ModelSerializer):
+    '''used to get information of emergency contacts'''
+    class Meta:
+        model = EmergencyContact
+        fileds = ['first_name', 'last_name', 'relationship', 'address', 'email', 'cell_phone', 'patient']
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    category = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field='name')
+
+    class Meta:
+
+        model = Category
+        fields = ['category', 'name']
+
+class ArticleSerializer(serializers.ModelSerializer):
+    '''get article and post from craweld data or create a new'''
+    class Meta:
+        model = Article
+        fields = ['article_id', 'author', 'categories', 'tags', 'created_at', 'updated_at']
+    def validate(self, attrs):
+        pass
+        # return super().validate(attrs)
+
+    # def get_categories(self, obj):
+    # '''obj is an agent instance.'''
+
+    #     qset = Category.objects.filter(name=obj)
+    #     return [CategorySerializer(m).data for m in qset]
+
+class DiseaseSerializer(serializers.ModelSerializer):
+    '''serialize Disease of users'''
+    class Meta:
+        model = Disease
+        fields = ['patient', 'disease_name', 'no_of_symp', 'symptoms_name', 'confidence', 'consultdoctor', 'allargis' ]
