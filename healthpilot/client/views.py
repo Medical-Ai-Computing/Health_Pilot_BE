194405c94fae48900976089e54f25f63378507ef
menilik <spyxmeni@gmail.com>
2023-05-10 00:09:44 +0300

    create api view for doctors

--- healthpilot/client/views.py
+++ healthpilot/client/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from rest_framework.views import APIView
@@ -9,8 +9,8 @@ import logging
 from django.db.models import Count
 from django.http import QueryDict
 from django.utils import timesince, timezone
-from .models import User, EmergencyContact, Disease, Article
-from .serializers import UserSerializer
+from .models import User, EmergencyContact, Disease, Article, Doctor
+from .serializers import UserSerializer, DiseaseSerializer, ArticleSerializer,EmergencyContactSerializer, DoctorsSerializer
 
 class UserAPIView(mixins.CreateModelMixin,
                        mixins.RetrieveModelMixin,
@@ -25,19 +25,38 @@ class ArticleAPIView(mixins.CreateModelMixin,
                        mixins.RetrieveModelMixin,
                        mixins.ListModelMixin,
                        GenericViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 
 class EmergencyContactAPIView(mixins.CreateModelMixin,
                        mixins.RetrieveModelMixin,
                        mixins.ListModelMixin,
                        GenericViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+    serializer_class = EmergencyContactSerializer
+    queryset = EmergencyContact.objects.all()
 
 class DiseaseAPIView(mixins.CreateModelMixin,
-                       mixins.RetrieveModelMixin,
-                       mixins.ListModelMixin,
-                       GenericViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+                    mixins.UpdateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    GenericViewSet):
+    serializer_class = DiseaseSerializer
+    queryset = Disease.objects.all()
+
+
+class DoctorViewSet(mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.ListModelMixin,
+                  mixins.DestroyModelMixin,
+                  GenericViewSet):
+    ''' api view method for doctors
+    1. Create Doctors
+    2. List doctors
+    3. Retrive doctors
+    4. Update
+    5. Remove Doctors'''
+
+    serializer_class = DoctorsSerializer
+    queryset = Doctor.objects.all()
+
