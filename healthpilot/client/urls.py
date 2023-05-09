7abb5352003ebda187f468dc1828c4e5076eef1e
menilik <spyxmeni@gmail.com>
2023-05-10 00:09:19 +0300

    improve and add doctor urls

--- healthpilot/client/urls.py
+++ healthpilot/client/urls.py
@@ -6,19 +6,20 @@ from .views import *
 
 router = DefaultRouter()
 router.register('users', UserAPIView, basename='users')
-router.register('emergency_contact', UserAPIView, basename='emergency')
 router.register('disease', UserAPIView, basename='disease')
 router.register('article', UserAPIView, basename='articles')
+router.register('emergency_contact', UserAPIView, basename='emergency')
+router.register('doctors', DoctorViewSet, basename='doctors')
 
 
-emergency_contact_router = NestedDefaultRouter(router, 'emergency_contact', lookup='user')
-emergency_contact_router.register('emergency_contact', UserAPIView, basename='emergency_contact')
+emergency_contact_router = NestedDefaultRouter(router, 'users', lookup='user')
+emergency_contact_router.register('emergency_contact', EmergencyContactAPIView, basename='emergency_contact')
 
-disease_router = NestedDefaultRouter(router, 'disease', lookup='user')
-disease_router.register('disease', UserAPIView, basename='disease')
+disease_router = NestedDefaultRouter(router, 'users', lookup='user')
+disease_router.register('disease', DiseaseAPIView, basename='disease')
 
-article_router = NestedDefaultRouter(router, 'article', lookup='user')
-article_router.register('article', UserAPIView, basename='articles')
+article_router = NestedDefaultRouter(router, 'users', lookup='user')
+article_router.register('article', ArticleAPIView, basename='articles')
 
 
 urlpatterns = [
@@ -26,5 +27,6 @@ urlpatterns = [
     path('', include(emergency_contact_router.urls)),
     path('', include(disease_router.urls)),
     path('', include(article_router.urls)),
+
     
 ]
