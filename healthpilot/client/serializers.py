ce94264c7005640fc0813c5ebb92fb92d5e54d09
menilik <spyxmeni@gmail.com>
2023-05-11 23:29:18 +0300

    alter wigh and height fileds

--- healthpilot/client/serializers.py
+++ healthpilot/client/serializers.py
@@ -6,14 +6,16 @@ class UserSerializer(serializers.ModelSerializer):
     '''Used to get and update users'''
     class Meta:
         model = User
-        fields = ['id','username', 'full_name', 'date_of_birth', 'age', 'gender', 'membership']
+        fields = ['id','username', 'full_name', 'date_of_birth', 'age', 
+                  'weight', 'height','gender', 'membership']
         read_only_fields = ['id']
 
 class EmergencyContactSerializer(serializers.ModelSerializer):
     '''used to get information of emergency contacts'''
     class Meta:
         model = EmergencyContact
-        fileds = ['first_name', 'last_name', 'relationship', 'address', 'email', 'cell_phone', 'patient']
+        fileds = ['first_name', 'last_name', 'relationship', 'address', 
+                  'email', 'cell_phone', 'patient']
 
 class CategorySerializer(serializers.ModelSerializer):
 
