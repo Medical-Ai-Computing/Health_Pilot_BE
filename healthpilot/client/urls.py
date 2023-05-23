fcba5443a3696568036cfa3979aa296c468b4da5
menilik <spyxmeni@gmail.com>
2023-05-23 07:40:48 +0300

    api added, calling improved with specific ids

--- healthpilot/client/urls.py
+++ healthpilot/client/urls.py
@@ -7,27 +7,21 @@ from .views import *
 router = DefaultRouter()
 router.register('users', UserAPIView, basename='users')
 router.register('disease', DiseaseViewSet, basename='disease')
-router.register('article', ArticleAPIView, basename='articles')
+router.register('article', ArticleAPIView, basename='articles') #TODO some errors .validate() should return the validated data, AssertionError
 router.register('emergency_contact', EmergencyContactAPIView, basename='emergency_contact')
-router.register('users/<int:user_id>/emergency_contact/', UserEmergencyContactAPIView, basename='emergency_contact')
+# router.register('users_emergency', UserEmergencyContactAPIView, basename='user_emergency_contact')
+router.register('users/(?P<users_pk>[^/.]+)/users_emergency', UserEmergencyContactAPIView, basename='user_emergency_contact')
 router.register('doctors', DoctorViewSet, basename='doctors')
+router.register(r'health_assessment_sections', HealthAssessmentSectionViewSet, basename='user_health_assessment')
+router.register(r'medications', MedicationViewSet, basename='medications' )
 
-
-# emergency_contact_router = NestedDefaultRouter(router, 'emergency_contact', lookup='emergency_contact')
-# emergency_contact_router.register('', EmergencyContactAPIView, basename='emergency_contact')
-
-# disease_router = NestedDefaultRouter(router, 'disease', lookup='disease')
-# disease_router.register('', DiseaseViewSet, basename='disease')
-
-# article_router = NestedDefaultRouter(router, 'article', lookup='article')
-# article_router.register('', ArticleAPIView, basename='articles') #TODO some errors .validate() should return the validated data, AssertionError
+emergency_contact_router = NestedDefaultRouter(router, 'users', lookup='users')
+emergency_contact_router.register('users_emergency', UserEmergencyContactAPIView, basename='user_emergency_contact')
 
 
 urlpatterns = [
     path('', include(router.urls)),
     # path('', include(emergency_contact_router.urls)),
-    # path('', include(disease_router.urls)),
-    # path('', include(article_router.urls)),
     path('payment/', PaymentCreateView.as_view(), name='payment'),
     path('profile/', UserProfileView.as_view(), name='profile'),
 
