5de1b60dd04143b40709d216afa10d8b0bc158ad
menilik <spyxmeni@gmail.com>
2023-05-23 18:06:44 +0300

    remove and update code

--- healthpilot/client/urls.py
+++ healthpilot/client/urls.py
@@ -9,19 +9,17 @@ router.register('users', UserAPIView, basename='users')
 router.register('disease', DiseaseViewSet, basename='disease')
 router.register('article', ArticleAPIView, basename='articles') #TODO some errors .validate() should return the validated data, AssertionError
 router.register('emergency_contact', EmergencyContactAPIView, basename='emergency_contact')
-# router.register('users_emergency', UserEmergencyContactAPIView, basename='user_emergency_contact')
-router.register('users/(?P<users_pk>[^/.]+)/users_emergency', UserEmergencyContactAPIView, basename='user_emergency_contact')
+router.register('users/(?P<users_id>[^/.]+)/users_emergency', UserEmergencyContactrViewset, basename='user_emergency_contact')
 router.register('doctors', DoctorViewSet, basename='doctors')
 router.register(r'health_assessment_sections', HealthAssessmentSectionViewSet, basename='user_health_assessment')
 router.register(r'medications', MedicationViewSet, basename='medications' )
 
-emergency_contact_router = NestedDefaultRouter(router, 'users', lookup='users')
-emergency_contact_router.register('users_emergency', UserEmergencyContactAPIView, basename='user_emergency_contact')
+# emergency_contact_router = NestedDefaultRouter(router, 'users', lookup='users')
+# emergency_contact_router.register('users_emergency', UserEmergencyContactrViewset, basename='user_emergency_contact')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('', include(emergency_contact_router.urls)),
     path('payment/', PaymentCreateView.as_view(), name='payment'),
     path('profile/', UserProfileView.as_view(), name='profile'),
 
