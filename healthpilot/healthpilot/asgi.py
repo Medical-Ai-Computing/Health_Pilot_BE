9b87acd28365b55da5b17930c60f17ded4734cba
menilik <spyxmeni@gmail.com>
2023-05-29 09:05:57 +0300

    improve websocker with daphne server for chat sys

--- healthpilot/healthpilot/asgi.py
+++ healthpilot/healthpilot/asgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from chat.consumers import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthpilot.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': URLRouter([
+        path('ws/chat/<int:chat_id>/', ChatConsumer.as_asgi()),
+    ]),
+})
