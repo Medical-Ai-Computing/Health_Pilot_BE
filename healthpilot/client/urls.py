6c71497879f9046549fb11f890a925c04f980d08
menilik <spyxmeni@gmail.com>
2023-06-10 01:21:03 +0300

    change profile api and improve apis

--- healthpilot/client/urls.py
+++ healthpilot/client/urls.py
@@ -7,21 +7,18 @@ from .views import *
 router = DefaultRouter()
 router.register('users', UserAPIView, basename='users')
 router.register('disease', DiseaseViewSet, basename='disease')
-router.register('article', ArticleAPIView, basename='articles') #TODO some errors .validate() should return the validated data, AssertionError
+router.register('article', ArticleAPIView, basename='articles')
 router.register('emergency_contact', EmergencyContactAPIView, basename='emergency_contact')
 router.register('users/(?P<users_id>[^/.]+)/users_emergency', UserEmergencyContactViewset, basename='user_emergency_contact')
-router.register('doctors', DoctorViewSet, basename='doctors')
+router.register('user_doctors', UserDoctorViewSet, basename='user_doctors')
+router.register('users/(?P<users_id>[^/.]+)/user_doctors', SingleUserDoctorViewset, basename='user_doctors_list')
 router.register('health_assessment_sections', HealthAssessmentSectionViewSet, basename='user_health_assessment')
 router.register('medications', MedicationViewSet, basename='medications' )
-
-# emergency_contact_router = NestedDefaultRouter(router, 'users', lookup='users')
-# emergency_contact_router.register('users_emergency', UserEmergencyContactrViewset, basename='user_emergency_contact')
+router.register('profile', UserProfileViewSet, basename='profile')
 
 
 urlpatterns = [
     path('', include(router.urls)),
     path('payment/', PaymentCreateView.as_view(), name='payment'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
 
-    
 ]
