1ec8331b2f4315d7993767f889b953e02b8128a7
menilik <spyxmeni@gmail.com>
2023-06-29 23:15:39 +0300

    load user conversation and feed it to chatbot

--- healthpilot/chat/views.py
+++ healthpilot/chat/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAdminUser
 from asgiref.sync import async_to_sync
 
-from .openai_chat import get_completion_from_messages, collect_messages
+from .openai_chat import get_completion_from_messages, collect_messages, context
 
 from client.models import User, UserProfile
 from .models import PrivateChat, GroupChat, Message, Conversation, ChatbotMessage
@@ -108,28 +108,47 @@ class ChatbotMessageViewSet(viewsets.ModelViewSet):
                             status=status.HTTP_400_BAD_REQUEST)
 
         user_input = request.data['context']
+        print('************', user_input)
+        chatbot_messages = ''
         try:
             conversation = Conversation.objects.get(user=user)
+            # Get the previous chatbot messages
+            chatbot_messages = ChatbotMessage.objects.filter(conversation=conversation).order_by('timestamp')
+
         except Conversation.DoesNotExist:
             print('Creating Conversation instance---------')
             conversation = Conversation.objects.create(user=user)
+
+        for message in chatbot_messages:
+            if message.sender == request.data['sender']:
+                print('user message')
+                context.append({'role': 'user', 'content': message.context})
+            elif message.sender == 'assistant':
+                print('assistance message')
+                context.append({'role': 'assistant', 'content': message.context})
+
+        # Append the current user input to the context
+        context.append({'role': 'user', 'content': user_input})
+        print('```````', context)
+        # Process user's message and generate chatbot's response
+        response = collect_messages(user_input)
+        response = get_completion_from_messages(response)
+
         # Save user's message
         user_message = ChatbotMessage.objects.create(
             conversation=conversation,
             sender=user.id,
             context=user_input
         )
-        # Process user's message and generate chatbot's response
-        response = collect_messages(f'Hello my name is {user_name} ,' + user_input)
-        response = get_completion_from_messages(response)
 
         # Save chatbot's response
         chatbot_message = ChatbotMessage.objects.create(
-            conversation=conversation,
-            sender='Assistant',
-            context=response
-        )
+                                            conversation=conversation,
+                                            sender='assistant',
+                                            context=response)
+
         return Response(
-            {'input': user_message.context, 'message': chatbot_message.context},
-            status=status.HTTP_201_CREATED
+            {'input': user_message.context, 
+             'message': chatbot_message.context},
+                        status=status.HTTP_201_CREATED
         )
