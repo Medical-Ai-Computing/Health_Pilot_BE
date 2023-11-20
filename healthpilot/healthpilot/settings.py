39072ed3f11a1da48b7d683ff103cc1578286df2
menilik <spyxmeni@gmail.com>
2023-11-20 12:32:28 +0300

    improve ports and connections

--- healthpilot/healthpilot/settings.py
+++ healthpilot/healthpilot/settings.py
@@ -11,8 +11,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [ 
-                        'http://localhost:9000', 'http://127.0.0.1:9000', 'https://ec_back.ecombinators.com',
-                        'https://stagingrep.mindplex.ai', 'https://staginghealth.mindplex.ai']
+                        'http://localhost:9000', 'http://127.0.0.1:9000',
+                        'https://staginghealth.mindplex.io']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
